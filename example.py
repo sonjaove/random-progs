13dd51b53118291e2e6bc68fba38f6a33a491fde
sonjaove <the.seven.year.old.ove@gmail.com>
2025-04-08 11:39:29 +0530

    point tracing the circle.

--- example.py
+++ example.py
@@ -1,19 +1,17 @@
 from manim import *
 
-class PointMovingOnShapes(Scene):
+class PointMovingWithTrace(Scene):
     def construct(self):
         circle = Circle(radius=1, color=BLUE)
         dot = Dot()
-        dot2 = dot.copy().shift(RIGHT)
-        self.add(dot)
+        trace = TracedPath(dot.get_center, stroke_color=YELLOW, stroke_width=2)
 
-        line = Line([3, 0, 0], [5, 0, 0])
-        # wahts the point of this line?
-        # self.add(line)
+        self.add(trace, dot)
+
+        dot2 = dot.copy().shift(RIGHT)
 
         self.play(GrowFromCenter(circle))
         self.play(Transform(dot, dot2))
         self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
-        self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
-        # its tracing an extra circle, about a given point, in given time.
+        self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=2)
         self.wait()
